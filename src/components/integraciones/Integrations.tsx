192b1a97dfaf7a2a2dc5759b74b294ddd533fab9
lucas bianchi <lucmbianchi2000@gmail.com>
2024-08-27 13:33:23 -0300

    se agrega seccion de integraciones

--- src/components/integraciones/Integrations.tsx
+++ src/components/integraciones/Integrations.tsx
@@ -32,8 +32,8 @@ const Integrations = () => {
 
   return (
     <section className="relative bg-[#E4EDF7] z-40  w-full sectionStyle bottomSection">
-      <div className="container flex justify-between gap-2">
-        <div className="flex-1 grid grid-cols-3 grid-rows-3 gap-4">
+      <div className="container flex flex-col-reverse gap-5 md:flex-row md:justify-between md:gap-2">
+        <div className="flex-1 grid grid-cols-3 grid-rows-3 gap-2 md:gap-4">
           {integrations.map((integration, index) => (
             <IntegrationItem
               key={index}
@@ -43,9 +43,9 @@ const Integrations = () => {
             />
           ))}
         </div>
-        <div className="flex-1 space-y-4 flex flex-col items-end">
-          <p className="text-right subtitle">Especialistas</p>
-          <h2 className="text-[44px] text-right md:text-[44px] title ">
+        <div className="flex-1 space-y-4 flex flex-col items-start md:items-end">
+          <p className="text-left md:text-right subtitle">Especialistas</p>
+          <h2 className="text-[44px] text-left md:text-right md:text-[44px] title ">
             <span className="text-[#172B4D] w-full">
               Integra la Firma Electrónica
             </span>
@@ -59,19 +59,11 @@ const Integrations = () => {
               con tus Aplicaciones y CRMs
             </span>
           </h2>
-          <p className="text-right">
+          <p className="text-left md:text-right">
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia
             veniam sed doloremque minus dolore aliquid temporibus quaerat, quam
             perspiciatis porro itaque eveniet hic natus magnam.
           </p>
-          <div className="pt-2">
-            <Button
-              backgroundColor={"#2A5189"}
-              textColor={"white"}
-              content={"Hablar con especialista"}
-              borderColor="#2A5189"
-            />
-          </div>
         </div>
       </div>
     </section>
