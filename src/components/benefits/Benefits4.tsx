a6da8f4d78adee2bdaf53534a9676a98e84a6bdb
lucas bianchi <lucmbianchi2000@gmail.com>
2024-09-08 19:40:36 -0300

    cambios a main

--- src/components/benefits/Benefits4.tsx
+++ src/components/benefits/Benefits4.tsx
@@ -1,15 +1,17 @@
 "use client";
-import React from "react";
 import Image from "next/image";
-import { BiMessageRoundedDetail } from "react-icons/bi";
-import { GrMoney } from "react-icons/gr";
-import { IoAccessibility, IoDocumentsOutline } from "react-icons/io5";
-import {
-  MdOnlinePrediction,
-  MdOutlineIntegrationInstructions,
-} from "react-icons/md";
-import { VscLaw } from "react-icons/vsc";
+import React from "react";
+
 import { motion, useInView } from "framer-motion";
+import Benefits from "@/components/Benefits";
+import MemoryTwoToneIcon from "@mui/icons-material/MemoryTwoTone";
+import BalanceTwoToneIcon from "@mui/icons-material/BalanceTwoTone";
+import AccessibilityNewTwoToneIcon from "@mui/icons-material/AccessibilityNewTwoTone";
+import DocumentScannerTwoToneIcon from "@mui/icons-material/DocumentScannerTwoTone";
+import IntegrationInstructionsTwoToneIcon from "@mui/icons-material/IntegrationInstructionsTwoTone";
+import AttachMoneyTwoToneIcon from "@mui/icons-material/AttachMoneyTwoTone";
+import { SvgIconProps } from "@mui/material";
+import { BiMessageRoundedDetail } from "react-icons/bi";
 
 const Benefits4 = () => {
   const benefits = [
@@ -17,56 +19,54 @@ const Benefits4 = () => {
       title: "Transformación Digital",
       description:
         "Transforma tu empresa con soluciones digitales innovadoras que optimizan procesos y mejoran la eficiencia operativa.",
-      icon: MdOnlinePrediction,
+      icon: MemoryTwoToneIcon,
     },
     {
       title: "Validación Jurídica",
       description:
         "Asegura que todos tus documentos y procesos cumplan con los requisitos legales vigentes, protegiendo a tu empresa de riesgos legales.",
-      icon: VscLaw,
+      icon: BalanceTwoToneIcon,
     },
     {
       title: "Accesibilidad",
       description:
         "Facilita el acceso y manejo de tus documentos desde cualquier dispositivo, en cualquier lugar, asegurando disponibilidad continua.",
-      icon: IoAccessibility,
+      icon: AccessibilityNewTwoToneIcon,
     },
     {
       title: "Integridad Documental",
       description:
         "Garantiza la integridad, autenticidad y seguridad de tus documentos, evitando alteraciones no autorizadas y manteniendo su valor legal.",
-      icon: IoDocumentsOutline,
+      icon: DocumentScannerTwoToneIcon,
     },
     {
       title: "Integración Multicanal",
       description:
         "Conecta tus procesos a través de múltiples canales y plataformas, asegurando una experiencia fluida y sin interrupciones para tus usuarios.",
-      icon: MdOutlineIntegrationInstructions,
+      icon: IntegrationInstructionsTwoToneIcon,
     },
     {
       title: "Alto Retorno de Inversión",
       description:
         "Maximiza el retorno de inversión mediante soluciones eficaces y eficientes que impulsan el crecimiento y la rentabilidad de tu empresa.",
-      icon: GrMoney,
+      icon: AttachMoneyTwoToneIcon,
     },
   ];
 
-  // Crear referencias y estados de visibilidad para cada beneficio
-  const refs = benefits.map(() => React.useRef(null));
-  const isInViewArray = refs.map((ref) =>
-    useInView(ref, { once: true, amount: 0.1 })
-  );
+  // const ref = React.useRef<HTMLDivElement>(null);
+  // const isInView = useInView(ref, { once: true, amount: 0.1 });
 
   return (
     <section className="relative w-full bg-[#FEFEFE] sectionStyle">
       <div className="container flex flex-col-reverse items-start md:h-[900px] gap-20 md:flex-row md:justify-between">
         <div className="relative z-10 grid flex-1 grid-cols-1 gap-8 md:grid-cols-2">
-          {benefits.map((benefit, index) => {
-            const isInView = isInViewArray[index];
+          {/* {benefits.map((benefit, index) => {
+            const ref = React.useRef<HTMLDivElement>(null);
+            const isInView = useInView(ref, { once: true, amount: 0.1 });
 
             return (
               <motion.article
-                ref={refs[index]}
+                ref={ref}
                 initial={{ opacity: 0, y: 20 }}
                 animate={{ opacity: isInView ? 1 : 0, y: isInView ? 0 : 20 }}
                 transition={{ duration: 0.5, delay: 0.5 }}
@@ -85,7 +85,15 @@ const Benefits4 = () => {
                 </p>
               </motion.article>
             );
-          })}
+          })} */}
+          {benefits.map((benefit, index) => (
+            <BenefitItem
+              key={index}
+              title={benefit.title}
+              description={benefit.description}
+              icon={benefit.icon}
+            />
+          ))}
         </div>
         <div className="relative h-full w-full md:w-[40%] ">
           <article className="md:sticky md:top-0 md:left-0 md:w-full ">
@@ -135,50 +143,31 @@ const Benefits4 = () => {
 
 export default Benefits4;
 
-{
-  /* Sección de texto con sticky */
+interface benefitProps {
+  icon: React.ComponentType<SvgIconProps>;
+  title: string;
+  description: string;
 }
-{
-  /* <div className="relative h-full bg-green-300 md:flex-1 h-min-screen ">
-          <article className="w-full sticky top-0 left-0 h-[200px] bg-red-200">
-            <div className="flex items-center justify-end gap-3">
-              <p className="text-right text-[hsl(215,76%,73%)] font-bold">
-                Lorem ipsum
-              </p>
-              <Image src="/tituloPasos.png" alt="tit" height={60} width={60} />{" "}
-            </div>
 
-            <h2
-              className="text-right text-[58px] font-bold leading-none"
-              style={{
-                background: "linear-gradient(to top, #000000, #4C96FF)",
-                WebkitBackgroundClip: "text",
-                WebkitTextFillColor: "transparent",
-              }}
-            >
-              Beneficios Lorem, ipsum.
-            </h2>
-            <div className="mt-5 text-right text-md space-y-7 text-zinc-800 ">
-              <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ullam
-                consequuntur nemo molestiae in. Quas eius sed placeat
-                consequuntur? Nisi impedit velit distinctio dolore doloremque
-                repellat, quidem cum minus libero ali
-              </p>
-              <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ullam
-                consequuntur nemo molestiae in. Quas eius sed placeat
-                consequuntur? Nisi impedit velit distinctio dolore doloremque
-                repellat, quidem cum minus libero ali
-              </p>
-
-              <span className="inline-flex gap-3 mt-20 py-2 px-5 bg-[#2A5189] text-white rounded-lg justify-end items-center">
-                <BiMessageRoundedDetail className="h-[30px] w-[30px]" />
-                <span className="text-lg font-semibold">
-                  Hablar con especialista
-                </span>
-              </span>
-            </div>
-          </article>
-        </div> */
-}
+const BenefitItem = ({ icon: Icon, title, description }: benefitProps) => {
+  const ref = React.useRef<HTMLDivElement>(null);
+  const isInView = useInView(ref, { once: true, amount: 0.1 });
+  return (
+    <motion.article
+      ref={ref}
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: isInView ? 1 : 0, y: isInView ? 0 : 20 }}
+      transition={{ duration: 0.5, delay: 0.5 }}
+      // key={index}
+      className="flex flex-col justify-center w-full gap-2 p-6 text-left bg-white border rounded-lg shadow-lg md:p-8 shadow-blue-700/20 border-blue-700/20 md:border-zinc-100 md:border-2 md:hover:border-blue-700/20 md:hover:shadow-xl md:hover:shadow-blue-700/20"
+    >
+      <Icon style={{ fontSize: "2.8rem" }} className="text-[#006BFC]" />
+      <h3 className="py-0 text-lg font-semibold text-black md:text-lg">
+        {title}
+      </h3>
+      <p className="text-base leading-tight text-pretty text-zinc-400">
+        {description}
+      </p>
+    </motion.article>
+  );
+};
