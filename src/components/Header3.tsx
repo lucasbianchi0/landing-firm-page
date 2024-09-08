90a7074069882ece6cee391f0cf1556e7812f0df
lucas bianchi <lucmbianchi2000@gmail.com>
2024-09-08 20:07:55 -0300

    se agregan cambios a main

--- src/components/Header3.tsx
+++ src/components/Header3.tsx
@@ -14,21 +14,23 @@ const Header3 = () => {
   return (
     <section className="relative w-full min-h-[70vh] md:min-h-[95vh] py-6 overflow-hidden z-0">
       <Navbar />
-      <div className="container h-full">
+      <div className="h-full ">
         <div className="flex flex-col h-full ">
-          <article className=" text-white w-full lg:w-[50%] flex flex-col gap-3 md:gap-6 pt-8 md:pt-16 relative z-10 ">
-            <h2 className="montserrat text-[56px] text-white md:text-[60px] font-bold leading-none">
-              <span className="">Firma Biométrica </span>
-              <br />
-              <span>Segura y Eficiente</span>
-            </h2>
+          <div className="container flex flex-col h-full">
+            <article className=" text-white w-full lg:w-[50%] flex flex-col gap-3 md:gap-6 pt-8 md:pt-16 relative z-10 ">
+              <h2 className="montserrat text-[56px] text-white md:text-[60px] font-bold leading-none">
+                <span className="">Firma Biométrica </span>
+                <br />
+                <span>Segura y Eficiente</span>
+              </h2>
 
-            <p>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Debitis
-              rem libero id quod repudiandae nemo nam. Perferendis quibusdam
-              ducimus, natus laboriosam libero recusandae culpa doloremque.
-            </p>
-            {/* <div className="z-10 flex gap-2 mt-4 text-md md:mt-6 md:text-lg ">
+              <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Debitis rem libero id quod repudiandae nemo nam. Perferendis
+                quibusdam ducimus, natus laboriosam libero recusandae culpa
+                doloremque.
+              </p>
+              {/* <div className="z-10 flex gap-2 mt-4 text-md md:mt-6 md:text-lg ">
               <Button
                 backgroundColor={"transparent"}
                 textColor={"white"}
@@ -42,13 +44,13 @@ const Header3 = () => {
                 borderColor="#2A5189"
               />
             </div> */}
-          </article>
-          <div className="montserrat w-full md:w-[80%] mx-auto grid grid-cols-1 md:grid-cols-3  gap-4 mt-16 relative z-20">
-            <Metric content="1500 firmas" target={1500} />
-            <Metric content="200 clientes contentos" target={200} />
-            <Metric content="Más de 10 años de experiencia" target={10} />
+            </article>
+            <div className="montserrat w-full md:w-[80%] mx-auto grid grid-cols-1 md:grid-cols-3  gap-4 mt-16 relative z-20">
+              <Metric content="1500 firmas" target={1500} />
+              <Metric content="200 clientes contentos" target={200} />
+              <Metric content="Más de 10 años de experiencia" target={10} />
+            </div>
           </div>
-
           <div className="relative z-20">
             <CompanyList />
           </div>
