4de45ef1d98069777d39a45fd81193a498cb8616
lucas bianchi <lucmbianchi2000@gmail.com>
2024-12-03 22:39:44 -0300

    se agregan cambios

--- src/components/integraciones/Integrations.tsx
+++ src/components/integraciones/Integrations.tsx
@@ -2,8 +2,34 @@ import React from "react";
 import IntegrationItem from "./IntegrationItem";
 import TextContent from "../reusable/TextContent";
 import TollOutlinedIcon from "@mui/icons-material/TollOutlined";
-import { integraciones, textoIntegraciones } from "@/data/6-integraciones";
+import { textoIntegraciones } from "@/data/6-integraciones";
 const Integrations = () => {
+  const integraciones = [
+    {
+      image: "/integraciones/sharepoint.png",
+    },
+    {
+      image: "/integraciones/dynamics.png",
+    },
+    {
+      image: "/integraciones/salesforce.webp",
+    },
+    {
+      image: "/integraciones/sap.png",
+    },
+    {
+      style: "col-span-2",
+      image: "/integraciones/gg2.png",
+    },
+    {
+      style: "col-span-2 row-start-3",
+      image: "/integraciones/servicenow.png",
+    },
+    {
+      style: "col-start-3 row-start-3",
+      image: "/integraciones/aws.png",
+    },
+  ];
 
   return (
     <section className="relative z-10 w-full sectionStyle ">
