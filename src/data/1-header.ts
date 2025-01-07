e99ab47e8e0f864e8f3c7af50c3f020f170f6879
Martin Arjona <martinarjona@accedra.com.ar>
2025-01-07 14:37:16 -0300

    textos imagenes

--- src/data/1-header.ts
+++ src/data/1-header.ts
@@ -3,6 +3,6 @@ export const header = [
     primeraLinea: "Firma Biométrica",
     segundaLinea: "Segura y Eficiente",
     descripcion:
-      " Lorem ipsum dolor sit amet consectetur, adipisicing elit. Debitis rem libero id quod repudiandae nemo nam. Perferendis quibusdam ducimus, natus laboriosam libero recusandae culpa doloremque.",
+      "",
   },
 ];
